78f29cffd84569aabea5142068e7740aa9bbb0f2
Damaryzz <damarys.meroz22@gmail.com>
2026-01-09 09:23:35 -0500

    Reorganizo

--- parcial 2/practica-serverless/ms-gateway/src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Gateway de Usuarios y Reseñas - Arquitectura Microservicios';
-  }
-}
--- parcial 2/practica-serverless/ms-resena/src/events/webhook-event.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface WebhookEvent {
-  event_id: string;
-  event_type: string;
-  timestamp: string;
-  idempotency_key: string;
-  payload: any;
-}
